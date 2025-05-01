f670e7c4b369dad57c1a73c290f5e28a62660db6
Aarogya-Parajuli <aarogyaparajuli07@gmail.com>
2025-05-01 21:47:21 +0545

    test

---
+++ script.js
@@ -0,0 +1,10 @@
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// 1ecbfb6747c1f879d80ea2600aa097df
+// const URL = 'https://api.weatherstack.com/current?access_key=1f59c44907c605874653b4e5e2922976&query=New York'
+
+// async function test(){
+//     const response = await fetch('https://api.weatherstack.com/current?access_key=1f59c44907c605874653b4e5e2922976&query=Kathmandu')
+//     console.log(response);
+//     const data = await response.json();
+//     console.log(data)
+// }
