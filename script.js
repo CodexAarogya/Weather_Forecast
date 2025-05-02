16c5ef717863e9f5337eee6030a79c02819b24f0
Aarogya-Parajuli <aarogyaparajuli07@gmail.com>
2025-05-02 15:43:59 +0545

    left-container update

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-// 1ecbfb6747c1f879d80ea2600aa097df
-// const URL = 'https://api.weatherstack.com/current?access_key=1f59c44907c605874653b4e5e2922976&query=New York'
+const input = document.querySelector('input')
+const search = document.querySelector('#searchIcon')
 
-// async function test(){
-//     const response = await fetch('https://api.weatherstack.com/current?access_key=1f59c44907c605874653b4e5e2922976&query=Kathmandu')
-//     console.log(response);
-//     const data = await response.json();
-//     console.log(data)
-// }
+function getInput(){
+    console.log(input.value);
+     
+}
+
+input.addEventListener('keypress', (event)=>{
+    if(event.key === 'Enter'){
+        event.preventDefault();
+        getInput();
+    }
+})
+
+search.addEventListener('click',getInput);
+
+
+
+
+async function fetchData() {
+    try {
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=mbai&appid=1ecbfb6747c1f879d80ea2600aa097df&units=metric')
+
+        const data = await response.json();
+
+        if (response.ok) {
+
+            console.log(response);
+            console.log(data)
+
+        }
+        else {
+            console.log(data.message)
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
